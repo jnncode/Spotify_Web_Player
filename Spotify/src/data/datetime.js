219cb44c3ebbce75f99abc8b54264c2c370045bc
J Nguyen <n.nguyenj.media@proton.me>
2023-11-24 11:13:05 -0600

    Foundation

---
+++ Spotify/src/data/datetime.js
@@ -0,0 +1,18 @@
+// Defines appropriate greeting based on the time of the day
+document.addEventListener("DOMContentLoaded", function() {
+    const greetingsContainer = document.getElementById('greetings');
+    setGreetings(greetingsContainer);
+})
+
+function setGreetings(container) {
+    const currentHour = new Date().getHours(); // 24-hour config
+    let greeting; 
+    if (currentHour >= 5 && currentHour < 12) {
+        greeting = "Good morning";
+    } else if (currentHour >= 12 && currentHour < 18) {
+        greeting = "Good afternoon";
+    } else {
+        greeting = "Good evening";
+    }
+    container.textContent = greeting; // update content of container
+}
